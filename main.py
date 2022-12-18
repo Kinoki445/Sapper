4fdd38d0fe1abcc195b63677c178df0593404e35
Kinoki445 <Kinoki445@gmail.com>
2022-12-19 02:13:54 +0500

    Реализовал ООП

--- main.py
+++ main.py
@@ -1,25 +1,40 @@
 import tkinter
 
-window = tkinter.Tk()
 
-ROW = 4
-COLUMNS = 8
-
-buttons = []
-for i in range(ROW): #Создаём цикл который делает кнопки
-    temp = []
-    for j in range(COLUMNS):
-        btn = tkinter.Button(window, width = 4, font='Comics_sans 15 bold')
-        temp.append(btn)
-    buttons.append(temp)
-
-for row_btn in buttons:
-    print(row_btn)
-
-for i in range(ROW):
-    for j in range(COLUMNS):
-        btn = buttons[i][j] # Указывает место кнопки [0][0] начало
-        btn.grid(row =i, column = j) # Выводит в tinker сами кнопки 
+class BaronBunny:
+    window = tkinter.Tk()
+    ROW = 4
+    COLUMNS = 8
 
 
-window.mainloop()
+    def __init__(self):
+        self.buttons = []
+        for i in range(BaronBunny.ROW): #Создаём цикл который делает кнопки
+            temp = []
+            for j in range(BaronBunny.COLUMNS):
+                btn = tkinter.Button(BaronBunny.window, width = 4, font='Comics_sans 15 bold')
+                temp.append(btn)
+            self.buttons.append(temp)
+
+    def create_widgets(self):
+        for i in range(BaronBunny.ROW):
+            for j in range(BaronBunny.COLUMNS):
+                btn = self.buttons[i][j] # Указывает место кнопки [0][0] начало
+                btn.grid(row =i, column = j) # Выводит в tinker сами кнопки 
+
+    def start(self):
+        self.create_widgets()
+        BaronBunny.window.mainloop()
+
+
+game = BaronBunny()
+game.start()
+
+# for row_btn in game.buttons:
+#     print(row_btn)
+
+
+
+
+
+
